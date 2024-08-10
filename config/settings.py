9e4ecb0962ebd5e63655eb4970ad7b66ef3177ad
Valeev Azamat <vazamat@gmail.com>
2024-08-10 22:33:01 +0300

    django files after startapp and basic settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "catalog",
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
